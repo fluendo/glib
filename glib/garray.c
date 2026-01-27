16ba2942fd7571853f0d1f0de3346dc4cc12b603
Fabian Orccon <cfoch.fabian@gmail.com>
2026-01-27 16:48:29 +0100

    Revert "garray: Fix function signature by proxying"
    This reverts commit 0c4dbf4f2538f26b611347f021004bb2052f0e84.

--- glib/garray.c
+++ glib/garray.c
@@ -2500,8 +2500,8 @@ g_ptr_array_sort (GPtrArray    *array,
     g_qsort_with_data (array->pdata,
                        array->len,
                        sizeof (gpointer),
-                       g_compare_func_to_compare_data_func,
-                       compare_func);
+                       (GCompareDataFunc)compare_func,
+                       NULL);
 }
 
 /* Please keep this doc-comment in sync with
