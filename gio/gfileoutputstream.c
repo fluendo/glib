a0768ab6f4ce8f86c113e57b7cc17285abea05f4
Jorge Zapata <jorgeluis.zapata@gmail.com>
2026-06-18 10:08:28 +0200

    Revert "Fix function pointer cast issues"
    This reverts commit 80a65a0dd8d61972ecffc64613825bd4e3abf260.

--- gio/gfileoutputstream.c
+++ gio/gfileoutputstream.c
@@ -54,8 +54,7 @@
  * stream, use g_seekable_truncate().
  **/
 
-static void       g_file_output_stream_seekable_iface_init    (GSeekableIface       *iface,
-                                                               gpointer              iface_data);
+static void       g_file_output_stream_seekable_iface_init    (GSeekableIface       *iface);
 static goffset    g_file_output_stream_seekable_tell          (GSeekable            *seekable);
 static gboolean   g_file_output_stream_seekable_can_seek      (GSeekable            *seekable);
 static gboolean   g_file_output_stream_seekable_seek          (GSeekable            *seekable,
@@ -95,8 +94,7 @@ g_file_output_stream_class_init (GFileOutputStreamClass *klass)
 }
 
 static void
-g_file_output_stream_seekable_iface_init (GSeekableIface *iface,
-                                          gpointer        iface_data)
+g_file_output_stream_seekable_iface_init (GSeekableIface *iface)
 {
   iface->tell = g_file_output_stream_seekable_tell;
   iface->can_seek = g_file_output_stream_seekable_can_seek;
